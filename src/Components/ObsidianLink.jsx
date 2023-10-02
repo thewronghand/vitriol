d64107ef410a6b9a8fe645dd4d5e45629ff40e4b
thewronghand <penfreak77@gmail.com>
2023-10-02 13:49:28 +0900

    fix: 링크 컴포넌트 컬러 로직 추가

--- src/Components/ObsidianLink.jsx
+++ src/Components/ObsidianLink.jsx
@@ -1,27 +1,58 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export default function ObsidianLink(props) {
   const navigate = useNavigate();
 
+  // 대상 URL에서 \| 패턴 제거
+  const cleanHref =
+    props.href && decodeURIComponent(props.href).replace(/\\?\|.*/, "");
+
+  const childrenArray = React.Children.toArray(props.children);
+  let childText = childrenArray.map((child) =>
+    typeof child === "string" ? child.split("|")[0] : child
+  );
+
+  const fileName = getFileName(cleanHref);
+
+  const exists = useSelector(
+    (state) =>
+      state.graphData.nodes.find((node) => node.id === fileName)?.exists
+  );
+
   // href 속성이 있는지 확인
   if (
-    props.href &&
-    typeof props.href === "string" &&
-    props.href.startsWith("/post/")
+    cleanHref &&
+    typeof cleanHref === "string" &&
+    cleanHref.startsWith("/post/")
   ) {
     return (
-      <a
-        href={props.href}
+      <ColoredAnchor
+        href={cleanHref}
         onClick={(e) => {
           e.preventDefault();
-          navigate(props.href);
+          // 정제된 URL로 navigate
+          navigate(cleanHref);
         }}
-        className="asdf"
+        exists={exists}
       >
-        {props.children}
-      </a>
+        {childText}
+      </ColoredAnchor>
     );
   }
 
-  return <a href={props.href}>{props.children}</a>;
+  return <ColoredAnchor href={cleanHref}>{childText}</ColoredAnchor>;
+}
+
+const ColoredAnchor = styled.a`
+  text-decoration: none;
+  color: ${(props) => (props.exists ? "purple" : "red")};
+`;
+
+function getFileName(path) {
+  // 마지막 '/' 이후의 문자열을 가져옵니다.
+  const parts = path.split("/");
+  return parts[parts.length - 1];
 }
