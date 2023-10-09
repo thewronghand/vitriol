db8e80b076e3bd90800bd07fd48ff70fa93a0525
thewronghand <penfreak77@gmail.com>
2023-10-10 03:37:35 +0900

    design: 스타일 적용

--- src/Page/PostDetails.jsx
+++ src/Page/PostDetails.jsx
@@ -2,15 +2,22 @@ import { useParams } from "react-router-dom";
 import Graph from "../Components/Graph";
 import { useState, useEffect } from "react";
 import useObsidianMarkdown from "../utils/hooks/useObsidianMarkdown";
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import rehypeRaw from "rehype-raw";
 import { convertObsidianLinks, removeConfigSection } from "../utils/userUtils";
 import remarkGfm from "remark-gfm";
+import styled from "@emotion/styled";
+import useDetectWindowWidth from "../utils/hooks/useDetectWindowWidth";
+import MobileGraph from "../Components/MobileGraph";
+import { StyledMarkdown } from "../utils/styles";
+
+const mobileWindowWidth = 650;
 
 export default function PostDetails({ data }) {
   const [content, setContent] = useState("");
   const { id } = useParams();
   const components = useObsidianMarkdown();
+  const windowWidth = useDetectWindowWidth();
+  const isMobile = windowWidth <= mobileWindowWidth;
 
   useEffect(() => {
     fetch(`/src/site/notes/${id}.md`)
@@ -29,28 +36,61 @@ export default function PostDetails({ data }) {
   console.log(components);
 
   return (
-    <div style={{ display: "flex", width: "100%", height: "100%" }}>
-      <div style={{ width: "50%" }}>
-        <Graph data={data} currentId={id} />
-      </div>
-      <div
-        style={{
-          minWidth: "50%",
-          maxWidth: "50%",
-          textAlign: "left",
-          padding: "15px",
-          overflowY: "scroll",
-        }}
-      >
-        <h1>{id}</h1>
-        <ReactMarkdown
+    <PostDetailsContainer>
+      <GraphContainer>
+        {isMobile ? (
+          <MobileGraph data={data} currentId={id} />
+        ) : (
+          <Graph data={data} currentId={id} />
+        )}
+      </GraphContainer>
+      <DocContainer>
+        <DocHeading>{id}</DocHeading>
+        <StyledMarkdown
           rehypePlugins={[rehypeRaw]}
           components={components}
           remarkPlugins={[remarkGfm]}
         >
           {content}
-        </ReactMarkdown>
-      </div>
-    </div>
+        </StyledMarkdown>
+      </DocContainer>
+    </PostDetailsContainer>
   );
 }
+
+const PostDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  @media (min-width: 650px) {
+    flex-direction: row;
+  }
+`;
+
+const GraphContainer = styled.div`
+  width: 100%;
+  height: 50vh;
+  @media (min-width: 650px) {
+    width: 50%;
+    height: 100vh;
+  }
+`;
+
+const DocHeading = styled.h1`
+  margin-bottom: 15px;
+  font-size: 48px;
+  padding: 15px 0;
+  border-bottom: 1px solid lightgray;
+`;
+
+const DocContainer = styled.div`
+  padding: 15px;
+  min-width: 100%;
+  max-width: 100%;
+  overflow-y: auto;
+  @media (min-width: 650px) {
+    min-width: 50%;
+    max-width: 50%;
+  }
+`;
