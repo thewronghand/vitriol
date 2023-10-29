396a8f09db4ede1ea6567db38c4c07e1f647978f
thewronghand <penfreak77@gmail.com>
2023-10-29 11:18:25 +0900

    refactor: 그래프 force 축소, 드래그 시 svg 영역을 벗어나지 않도록 수정

--- src/Components/Graph.jsx
+++ src/Components/Graph.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef } from "react";
-import * as d3 from "d3";
-import { useNavigate } from "react-router-dom";
-import "./Graph.css";
-import { linkDistance } from "../utils/graphUtils";
-import { COLOR } from "../utils/styles";
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+import { useNavigate } from 'react-router-dom';
+import './Graph.css';
+import { linkDistance } from '../utils/graphUtils';
+import { COLOR } from '../utils/styles';
 
 function Graph({ data, currentId }) {
-  console.log("rerendered");
+  console.log('rerendered');
   const svgRef = useRef(null);
   const navigate = useNavigate();
 
@@ -14,7 +14,7 @@ function Graph({ data, currentId }) {
     if (!svgRef.current) return;
 
     const svg = d3.select(svgRef.current);
-    svg.selectAll("*").remove();
+    svg.selectAll('*').remove();
 
     const svgWidth = svgRef.current.clientWidth;
     const svgHeight = svgRef.current.clientHeight;
@@ -22,17 +22,17 @@ function Graph({ data, currentId }) {
     const simulation = d3
       .forceSimulation(data.nodes)
       .force(
-        "link",
+        'link',
         d3
           .forceLink(data.links)
           .id((d) => d.id)
           .distance((link) => linkDistance(link, data))
       )
-      .force("charge", d3.forceManyBody().strength(-200))
-      .force("x", d3.forceX(svgWidth / 2))
-      .force("y", d3.forceY(svgHeight / 2))
+      .force('charge', d3.forceManyBody().strength(-200))
+      .force('x', d3.forceX(svgWidth / 2).strength(0.1))
+      .force('y', d3.forceY(svgHeight / 2).strength(0.1))
       .force(
-        "collide",
+        'collide',
         d3.forceCollide().radius((d) => {
           const linkedNodesCount = data.links.filter(
             (l) => l.source.id === d.id || l.target.id === d.id
@@ -41,28 +41,28 @@ function Graph({ data, currentId }) {
         })
       );
 
-    const g = svg.append("g");
+    const g = svg.append('g');
 
     const link = g
-      .append("g")
-      .attr("class", "links")
-      .selectAll("line")
+      .append('g')
+      .attr('class', 'links')
+      .selectAll('line')
       .data(data.links)
-      .join("line");
+      .join('line');
 
     const node = g
-      .append("g")
-      .attr("class", "nodes")
-      .selectAll("circle")
+      .append('g')
+      .attr('class', 'nodes')
+      .selectAll('circle')
       .data(data.nodes)
-      .join("circle")
-      .attr("r", (d) => {
+      .join('circle')
+      .attr('r', (d) => {
         const linkedNodesCount = data.links.filter(
           (l) => l.source.id === d.id || l.target.id === d.id
         ).length;
         return 5 * (1 + linkedNodesCount * 0.1);
       })
-      .attr("fill", (d) => {
+      .attr('fill', (d) => {
         if (d.id === currentId) {
           return d.exists === false
             ? COLOR.GRAPH.NEGATIVE_SECONDARY
@@ -70,32 +70,32 @@ function Graph({ data, currentId }) {
         }
         return d.exists === false ? COLOR.GRAPH.NEGATIVE : COLOR.GRAPH.PRIMARY;
       })
-      .attr("cursor", (d) => (d.exists === false ? "default" : "pointer"))
+      .attr('cursor', (d) => (d.exists === false ? 'default' : 'pointer'))
       .call(drag(simulation))
-      .on("click", (event, d) => {
+      .on('click', (event, d) => {
         if (d.exists !== false) {
           navigate(`/post/${d.id}`);
         }
       });
 
-    node.append("title").text((d) => d.id);
+    node.append('title').text((d) => d.id);
 
     const text = g
-      .append("g")
-      .attr("class", "labels")
-      .selectAll("text")
+      .append('g')
+      .attr('class', 'labels')
+      .selectAll('text')
       .data(data.nodes)
-      .join("text")
-      .attr("text-anchor", "middle")
-      .attr("dominant-baseline", "central")
+      .join('text')
+      .attr('text-anchor', 'middle')
+      .attr('dominant-baseline', 'central')
       .text((d) => d.label)
-      .attr("fill", (d) => {
+      .attr('fill', (d) => {
         if (d.exists === false) {
           return COLOR.TEXT.TERTIARY;
         }
         return COLOR.TEXT.PRIMARY;
       })
-      .attr("pointer-events", "none");
+      .attr('pointer-events', 'none');
     // Quadtree 설정하기
     const quadtree = d3
       .quadtree()
@@ -105,17 +105,17 @@ function Graph({ data, currentId }) {
       ])
       .addAll(data.nodes.map((d) => [d.x, d.y]));
 
-    simulation.on("tick", () => {
+    simulation.on('tick', () => {
       link
-        .attr("x1", (d) => d.source.x)
-        .attr("y1", (d) => d.source.y)
-        .attr("x2", (d) => d.target.x)
-        .attr("y2", (d) => d.target.y);
+        .attr('x1', (d) => d.source.x)
+        .attr('y1', (d) => d.source.y)
+        .attr('x2', (d) => d.target.x)
+        .attr('y2', (d) => d.target.y);
 
-      node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+      node.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
 
       text
-        .attr("x", (d) => {
+        .attr('x', (d) => {
           let tx = d.x;
           let ty = d.y - 15;
 
@@ -133,7 +133,7 @@ function Graph({ data, currentId }) {
 
           return tx;
         })
-        .attr("y", (d) => {
+        .attr('y', (d) => {
           const linkedNodesCount = data.links.filter(
             (l) => l.source.id === d.id || l.target.id === d.id
           ).length;
@@ -144,9 +144,22 @@ function Graph({ data, currentId }) {
 
     const zoomHandler = d3
       .zoom()
-      .scaleExtent([0.5, 5])
-      .on("zoom", (event) => {
-        g.attr("transform", event.transform);
+      .scaleExtent([1, 5])
+      .on('zoom', (event) => {
+        let transform = event.transform;
+
+        const width = svgWidth / transform.k;
+        const height = svgHeight / transform.k;
+
+        // 제한된 x와 y 값을 계산
+        const maxX = svgWidth - width;
+        const maxY = svgHeight - height;
+
+        // 이동할 수 있는 최대/최소 x와 y 값으로 transform.x와 transform.y 값을 제한
+        transform.x = Math.min(0, Math.max(-maxX, transform.x));
+        transform.y = Math.min(0, Math.max(-maxY, transform.y));
+
+        g.attr('transform', transform);
       });
     svg.call(zoomHandler);
 
@@ -158,8 +171,8 @@ function Graph({ data, currentId }) {
       }
 
       function dragged(event, d) {
-        d.fx = event.x;
-        d.fy = event.y;
+        d.fx = Math.min(svgWidth - 5, Math.max(5, event.x));
+        d.fy = Math.min(svgHeight - 5, Math.max(5, event.y));
       }
 
       function dragended(event, d) {
@@ -170,14 +183,14 @@ function Graph({ data, currentId }) {
 
       return d3
         .drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended);
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended);
     }
   }, [data, navigate, currentId]);
 
   return (
-    <div style={{ width: "100%", height: "100%", overflow: "hidden" }}>
+    <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
       <svg ref={svgRef} width="100%" height="100%" overflow="hidden"></svg>
     </div>
   );
